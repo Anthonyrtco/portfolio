47cd1aec5d93c234bfbf713b8c33060300443749
Anthonyrtco <anthony47330@hotmail.fr>
2023-03-08 10:00:51 +0100

    corrections et ajout api

---
+++ api/index.js
@@ -0,0 +1,21 @@
+'use strict'
+const { request } = require('express')
+/* eslint-env node, es6 */
+
+//IMPORTE LE PAQUET EXPRESS//
+const express = require('express')
+
+const PORT= 6300
+
+//Créé une application express//
+const app = express()
+
+//quatre methodes 'post'donner 'put'=remplacer 'delete'=supprimer 'get'=prendre//
+app.get ('/', (req, res)=> {
+    res.send('hello world')
+})
+
+app.listen(PORT, () =>{
+    console.log (`serveur démarré : file:///Users/anthonyrictio/Desktop/portfolio/pages/index.html:${PORT}`)
+})
+
